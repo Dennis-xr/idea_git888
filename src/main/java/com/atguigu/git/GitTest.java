bb068709df87ed040967356a6c083e5ee1204de6
xurui <ruixu981113@163.com>
2022-06-23 15:42:45 +0800

    分支dev的添加

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("new line");
         System.out.println("branch dev update");
+        System.out.println("dev update");
     }
 }
